a2a87f7d5d39fed66569c17cb936833ccf2d5d17
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2025-09-03 16:26:19 +0000

    remove langue

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { I18nextProvider } from "react-i18next";
 import '@ant-design/v5-patch-for-react-19';
 
 import { ToastContainer } from "react-toastify";
 const queryClient = new QueryClient();
 // import '@ant-design/v5-patch-for-react-19';
 import { SnackbarProvider } from "notistack";
-import i18n from "./i18";
 import Router from "./router/router";
 function App() {
   return (
-    <I18nextProvider i18n={i18n}>
       <SnackbarProvider
        maxSnack={3}
        anchorOrigin={{ vertical: "top", horizontal: "center" }}
@@ -25,7 +22,6 @@ function App() {
         <ToastContainer />
       </QueryClientProvider>
       </SnackbarProvider>
-    </I18nextProvider>
   );
 }
 
